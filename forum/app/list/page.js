a2d3b6cef96fb79ccc69c82fee93e93714223219
yeryunJung <2bites@naver.com>
2023-10-26 17:21:29 +0900

    Feat: db 데이터로 게시판 구현

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,18 +1,18 @@
+import { connectDB } from "@/util/database";
+
 export default async function List() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
   return (
     <div className="list-bg">
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
-      <div className="list-item">
-        <h4>글제목</h4>
-        <p>1월 1일</p>
-      </div>
+      {result.map((a, i) => (
+        <div className="list-item" key={i}>
+          <h4>{a.title}</h4>
+          <p>{a.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
